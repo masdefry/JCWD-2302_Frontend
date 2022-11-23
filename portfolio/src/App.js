bebb2a85d966937a2c4563b15fbd16a62119f1a5
masdefry <muhammaddefryan@gmail.com>
2022-11-23 15:29:25 +0700

    Portfolio Done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,20 +5,8 @@ import Navbar from './components/navbar';
 import Jumbotron from './components/jumbotron';
 import Aboutme from './components/aboutme';
 import Worked from './components/worked';
-import { useState } from 'react';
 
 function App() {
-  const [data, setData] = useState([
-    { year: '2020-2022', company: 'Purwadhika' },
-    { year: '2022-Pensiun', company: 'Telkomsel' }
-  ])
-
-  const [idx, setIdx] = useState([0])
-
-  let getData = (idx) => {
-    setIdx(idx)
-  }
-
   return (
     <div>
       {/* Section1: Navbar */}
@@ -31,46 +19,7 @@ function App() {
       <Aboutme />
 
       {/* Section4: Worked */}
-      {/* <Worked /> */}
-      <div className="worked">
-        <div className="title">
-            <h1>
-                02. Where Ive Worked
-            </h1>
-            <div className="line">
-            
-            </div>
-        </div>
-        <div className="sidebar">
-            <div >
-                Menu-1 
-            </div>
-            <div onClick={() => getData(1)}>
-                Menu-2
-            </div>
-            <div>
-                Menu-3
-            </div>
-            <div>
-                Menu-4
-            </div>
-            <div>
-                Menu-5
-            </div>
-        </div>
-        <div className="description">
-            {
-             <div>
-             <p className="title">
-                 {data[idx].company}
-             </p>
-             <p className="desc">
-               {data[idx].year}
-             </p>
-           </div>
-            }
-        </div>
-    </div>
+      <Worked />
     </div>
   );
 }
--- portfolio/src/components/worked.jsx
+++ portfolio/src/components/worked.jsx
@@ -1,47 +1,61 @@
-import React from 'react';
+import { useState } from 'react';
 
-class Worked extends React.Component{
-    render(){
-        return(
-            <div className="worked">
-                <div className="title">
-                    <h1>
-                        02. Where Ive Worked
-                    </h1>
-                    <div className="line">
-                    
-                    </div>
+function Worked(){
+    const [data, setData] = useState([
+        { year: '2020-2022', company: 'Purwadhika' },
+        { year: '2022-Pensiun', company: 'Telkomsel' }
+      ])
+    
+      const [idxSelected, setIdxSelected] = useState(0)
+    
+      let onChange = (idx) => {
+        setIdxSelected(idx)
+      }
+
+      let onMapping = () => {
+        return data.map((value, index) => {
+            return(
+                <div onClick={() => onChange(index)}>
+                    {value.year}
                 </div>
-                <div className="sidebar">
-                    <div>
-                        Menu-1 
-                    </div>
-                    <div>
-                        Menu-2
-                    </div>
-                    <div>
-                        Menu-3
-                    </div>
-                    <div>
-                        Menu-4
-                    </div>
-                    <div>
-                        Menu-5
-                    </div>
-                </div>
-                <div className="description">
-                    <div>
-                        <p className="title">
-                            Engineer @Purwadhika
-                        </p>
-                        <p className="desc">
-                            2020 - 2022
-                        </p>
-                    </div>
-                </div>
+            )
+        })
+      }
+    return(
+        <div className="worked">
+        <div className="title">
+            <h1>
+                02. Where Ive Worked
+            </h1>
+            <div className="line">
+            
             </div>
-        )
-    }
+        </div>
+        <div className="sidebar">
+            {
+                // onMapping()
+                data.map((value, index) => {
+                    return(
+                        <div onClick={() => onChange(index)}>
+                            {value.year}
+                        </div>
+                    )
+                })
+
+            }
+        </div>
+        <div className="description">
+             <div>
+             <p className="title">
+                 {data[idxSelected].company}
+             </p>
+             <p className="desc">
+               {data[idxSelected].year}
+             </p>
+           </div>
+        </div>
+      </div>
+    )
 }
 
 export default Worked
